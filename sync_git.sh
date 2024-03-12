c1c3271a0b6a190e7b1f248a52239423a2ffb551
tnemet <itnemet@protonmail.com>
2024-03-12 13:41:18 +0100

    OpenWrt test project

--- sync_git.sh
+++ sync_git.sh
@@ -7,7 +7,7 @@ git add interfaces.config
 git add diffconfig_x86
 git add sync_git.sh
 git add Proof_of_Delivery.pdf
-git add README.md
+git add mypackages
 git add new_pid
 git commit -m "OpenWrt test project"
-git push https://tnemet:key@github.com/tnemet/test_task.git master
+git push https://tnemet:key@github.com/tnemet/test_project.git main
